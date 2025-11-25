c2dab6c18466087e3e06ba6989eb1100f0dbc2ba
Dmitry Novikov <dredfort.42@gmail.com>
2025-11-25 19:44:17 +0000

    h files checked

--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,16 @@
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <unistd.h>
-# include <stdlib.h>
+#define GET_NEXT_LINE_H
 
-size_t	ft_strlen(const char *s);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-int		get_next_line(int fd, char **line);
-int		error(char *buffer);
-char	*next_line_from_storage(char *storage);
-char	*remove_line_from_storage(char *storage);
+#include <unistd.h>
+#include <stdlib.h>
+
+size_t ft_strlen(const char *s);
+void *ft_memmove(void *dst, const void *src, size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strchr(const char *s, int c);
+int get_next_line(int fd, char **line);
+int error(char *buffer);
+char *next_line_from_storage(char *storage);
+char *remove_line_from_storage(char *storage);
 
 #endif
